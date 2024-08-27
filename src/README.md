385324aa59f0bb0616234a2471f6fbf624a11751
austinseger <austin.seger.1024@gmail.com>
2024-08-26 17:22:55 -0700

    codes

---
+++ src/README.md
@@ -0,0 +1,37 @@
+# Code Gen
+  
+  ![License](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+  ## Description
+  Make codes
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  install
+  
+  ## Usage
+  use carefully
+  
+  ## License
+  
+  This application is covered under the MIT license. [Learn more about the MIT license](https://choosealicense.com/licenses/mit/)
+  
+  
+  ## Contributing
+  yes
+  
+  ## Tests
+  test
+  
+  ## Questions
+  If you have any questions, feel free to reach out:
+  - GitHub: [austinseger](https://github.com/austinseger)
+  - Email: [@email](mailto:@email)
+  
---
+++ src/index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Successfully generated README.md');
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  });
+}
+
+init();
